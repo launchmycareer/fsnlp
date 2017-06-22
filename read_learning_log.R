c09602f8d971294d4192f6eb73a5df6d99077163
mattsoldner <matt.soldner@gmail.com>
2017-06-22 11:54:38 -0400

    Created wordcloud syntax

--- read_learning_log.R
+++ read_learning_log.R
@@ -9,6 +9,7 @@
 
 library(tm)
 library(quanteda)
+library(RColorBrewer)
 
 # Set working directory and load all PDFs
 setwd("C:/Users/msoldner/Documents/GitHub/fsnlp/_sources")
@@ -19,5 +20,21 @@ Rpdf <- readPDF(control = list(text = "-layout"))
 
 # Read in logs
 learning.logs <- Corpus(URISource(files), readerControl = list(reader = Rpdf))
+learning.logs <- tm_map(learning.logs, removePunctuation)
 ll.corpus <- corpus(learning.logs)
 summary(ll.corpus)
+
+specific_stopwords <- c("communityâ-termsâ-andâ-conditions", " monthlyâ�current", "dctdecode")
+specific_stopwords <- c(specific_stopwords, "eduâ", "s", "ï", "http", "bmgf.force.com", "error")
+specific_stopwords <- c(specific_stopwords, "log", "filter", "â-")
+
+myStemMat <- dfm(ll.corpus, 
+  remove = c(stopwords("english"), specific_stopwords), 
+  remove_punct = TRUE,
+  remove_numbers = TRUE,
+  verbose = TRUE,
+  stem=TRUE)
+
+textplot_wordcloud(myStemMat, min.freq = 25)
+
+
